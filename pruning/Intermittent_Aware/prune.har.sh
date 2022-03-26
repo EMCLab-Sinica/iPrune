8c6f2a9c5a12d87ac3078aef08748a134e8b6015
Jacky Lin <ire33164@gmail.com>
2022-03-26 23:02:35 +0800

    [HAR] Update model HAR

--- pruning/Intermittent_Aware/prune.har.sh
+++ pruning/Intermittent_Aware/prune.har.sh
@@ -5,21 +5,23 @@ CANDIDATES_PRUNING_RATIOS='0.25 0.3 0.35 0.4'
 MY_DEBUG='--debug -1' # -1: none, 0: info, 1: debug
 PRUNE_COMMON_FLAGS='--prune '$PRUNE_METHOD' --sa '$MY_DEBUG
 			    # intermittent/energy
-# original training -- 92.43%
+'''
+# original training -- 92.50%
 python main.py $COMMON_FLAGS
-# 25% pruned -- 99.23/99.31
+'''
+# 25% pruned -- 92.16/92.20
 python main.py $COMMON_FLAGS $PRUNE_COMMON_FLAGS \
 	--stage 0 \
 	--pretrained saved_models/HAR.origin.pth.tar
-'''
-# 43.1% pruned -- 99.29/99.31
+# 43.1% pruned -- 92.37/92.67
 python main.py $COMMON_FLAGS $PRUNE_COMMON_FLAGS \
 	--stage 1 \
 	--pretrained saved_models/$PRUNE_METHOD/$Model/stage_0.pth.tar
-# 57.4% pruned -- 99.16/99.27
+# 57.4% pruned -- 92.26/92.03
 python main.py $COMMON_FLAGS $PRUNE_COMMON_FLAGS \
 	--stage 2 \
 	--pretrained saved_models/$PRUNE_METHOD/$Model/stage_1.pth.tar
+'''
 # 67.6% pruned -- 98.99/99.16
 python main.py $COMMON_FLAGS $PRUNE_COMMON_FLAGS \
 	--stage 3 \
